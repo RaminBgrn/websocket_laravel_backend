ed19cfe5a13f1c879077d4bc486088be2663bcc3
RaminBgrn <ramin.bagheriian@gmail.com>
2024-11-30 19:42:53 +0330

    register and login has been completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,28 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserController extends ResponseController
 {
-    public function index(){
-        return 'index';
-    }
+
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Register new user in app.
      */
     public function register(Request $request)
     {
+        return $request->all();
         $userDataValidation = $request->validate([
             'user_name' => "required|string",
             'email' => "required|email|string",
@@ -38,44 +30,45 @@ class UserController extends ResponseController
             'name' => $userDataValidation['user_name'],
             'email' => $userDataValidation['email'],
             'password' => Hash::make($userDataValidation['password']),
-            'uuid'=> $uuid,
+            'uuid' => $uuid,
         ]);
 
         $accessToken = $user->createToken($uuid)->plainTextToken;
 
-         return $this->successResponse(201 , $accessToken);
-
+        return $this->successResponse(201, $accessToken);
     }
 
     /**
-     * Display the specified resource.
+     * login with user name or email password.
      */
-    public function show(string $id)
+    public function login(Request $request)
     {
-        //
+        $userLoginValidation = $request->validate([
+            'email' => 'email|required',
+            'password' => "string|min:8",
+        ]);
+        $user = User::where("email" , $userLoginValidation['email'])->get()->first();
+
+
+        if(empty($user)){
+            return $this->errorResponse("User not found" , 404);
+        }
+
+        if(!Hash::check($userLoginValidation['password'] , $user->password)){
+            return $this->errorResponse('Unauthorized' , 403);
+        }
+
+        $accessToken = $user->createToken($user->uuid)->plainTextToken;
+        return $this->successResponse(201, $accessToken);
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * logout user .
      */
-    public function edit(string $id)
+    public function logout()
     {
-        //
+        Auth::user()->tokens()->delete();
+        return $this->successResponse(200, 'token deleted successfully');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,15 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\Authentication;
-use Illuminate\Auth\Middleware\Authenticate;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login' , [UserController::class , 'login']);
 Route::post('/register' , [UserController::class , 'register']);
 
+
+
+
 Route::middleware([Authentication::class])->group(function(){
     Route::get('/my' , [UserController::class , 'index']);
+    Route::get('/logout' , [UserController::class , 'logout']);
 });
